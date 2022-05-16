8466a71a3c0a10200f93d7daabf7eeaac559d0ef
neo <neo@kfsyscc.org>
2022-05-16 16:07:36 +0800

    centos-kafka + mysql connector

--- LogiKM/build.sh
+++ LogiKM/build.sh
@@ -1,2 +1,2 @@
-# sudo docker build -t logikm-mysql:5.7.37 -f container/dockerfiles/mysql/Dockerfile container/dockerfiles/mysql
+sudo docker build -t logikm-mysql:5.7.37 -f container/dockerfiles/mysql/Dockerfile container/dockerfiles/mysql
 sudo docker build -t logikm .
--- centos-kafka/run.sh
+++ centos-kafka/run.sh
@@ -4,6 +4,7 @@ sudo docker run -dit --name centos-kafka \
   --privileged=true \
   -v $PWD/data:/tmp \
   -p 2181:2181 \
+  -p 8083:8083 \
   -p 9092:9092 \
   -v /etc/localtime:/etc/localtime:ro \
   -t "centos-kafka"
